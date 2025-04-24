5ff66cfc44616eb9d39ecbde49871a6047e31519
김신명 <rlatlsaud438@gmail.com>
2025-04-24 14:46:19 +0900

    API 테스트

--- app.mjs
+++ app.mjs
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/posts", postRouter);
+app.use("/posts", postsRouter);
 app.use("/auth", authRouter);
 
 app.use((req, res, next) => {
--- router/posts.mjs
+++ router/posts.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import * as postControler from "../controller/post.mjs";
 
 const router = express.Router();
 
@@ -8,17 +9,27 @@ const router = express.Router();
 //http://127.0.0.1:8080/posts/
 //http://127.0.0.1:8080/posts?userid=apple
 
+router.get("/", postControler.getPosts);
+
 //글 번호에 대한 포스트 가져오기 (get)
 //http://127.0.0.1:8080/posts/:id
+
+router.get("/:id", postControler.getPost);
 
 //포스트 쓰기 (post)
 //http://127.0.0.1:8080/posts
 //json 형태로 입력 후 저장
 
+router.post("/", postControler.createPost);
+
 //포스트 수정하기 (put)
 //json형대로 입력 후 저장
 
+router.put("/:id", postControler.updatePost);
+
 //포스트 삭제하기 (delete)
 //http://127.0.0.1:8080/posts/:id
+
+router.delete("/:id", postControler.deletePost);
 
 export default router;
