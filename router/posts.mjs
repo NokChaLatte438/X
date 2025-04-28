6952ad49450e9712785809deb5eb703b4cae94ed
김신명 <rlatlsaud438@gmail.com>
2025-04-28 10:50:17 +0900

    auht

--- router/posts.mjs
+++ router/posts.mjs
@@ -1,7 +1,14 @@
 import express from "express";
 import * as postControler from "../controller/post.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
 
 const router = express.Router();
+
+const validatePost = [
+  body("text").trim().isLength({ min: 5 }).withMessage("최소 5자 입력"),
+  validate,
+];
 
 //모든 포스트 가져오기
 
@@ -20,12 +27,12 @@ router.get("/:id", postControler.getPost);
 //http://127.0.0.1:8080/posts
 //json 형태로 입력 후 저장
 
-router.post("/", postControler.createPost);
+router.post("/", validatePost, postControler.createPost);
 
 //포스트 수정하기 (put)
 //json형대로 입력 후 저장
 
-router.put("/:id", postControler.updatePost);
+router.put("/:id", validatePost, postControler.updatePost);
 
 //포스트 삭제하기 (delete)
 //http://127.0.0.1:8080/posts/:id
